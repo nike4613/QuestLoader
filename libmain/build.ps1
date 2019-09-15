b1001ee18048b2e6e2754296db53f2e55910fb9e
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-09-15 00:38:23 -0500

    build.ps1 no longer is dependent on cwd

--- libmain/build.ps1
+++ libmain/build.ps1
@@ -1,3 +1,3 @@
-$NDKPath = Get-Content ./ndkpath.txt
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
 
-& "$NDKPath/build/ndk-build.cmd" NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_APPLICATION_MK=./Application.mk
+& "$NDKPath/build/ndk-build.cmd" NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk

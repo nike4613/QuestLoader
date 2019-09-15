4c794b21ee2d47a01a91fce43b6c11468ab6bd58
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-09-15 00:38:29 -0500

    Fixed whitespace

--- libmain/Android.mk
+++ libmain/Android.mk
@@ -24,7 +24,7 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 LOCAL_LDLIBS    := -llog -ldl
 LOCAL_MODULE    := main
-LOCAL_CPPFLAGS 	:= -std=c++2a
+LOCAL_CPPFLAGS  := -std=c++2a
 
 LOCAL_C_INCLUDES := ./include ./src
 LOCAL_SRC_FILES  := $(call rwildcard,src/,*.cpp)

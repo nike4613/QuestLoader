556f57cb3abdd9fec43759c908b63712a3241907
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-09-19 18:08:16 -0500

    libmodloader now iterates and prints all Unity JNI registrations

--- libmodloader/src/modloader.cpp
+++ libmodloader/src/modloader.cpp
@@ -2,12 +2,38 @@
 #include "log.hpp"
 #include <cstddef>
 #include <cstdint>
+#include <span>
+#include <string>
 
-// holy fuck why won't this export
+#include <sys/mman.h>
+
+// there is *not* a good way to get the name lmao
+std::string get_jni_class_name(JNIEnv* env, jclass klass) {
+    
+
+    return {};
+}
+
 extern "C" JNINativeInterface modloader_main(JavaVM* vm, JNIEnv* env, std::string_view loadSrc) noexcept {
     logf(ANDROID_LOG_VERBOSE, "modloader_main called with vm: 0x%p, env: 0x%p, loadSrc: %s", vm, env, loadSrc.data());
 
-    return jni::interface::make_passthrough_interface<JNINativeInterface>(&env->functions);
+    auto iface = jni::interface::make_passthrough_interface<JNINativeInterface>(&env->functions);
+
+    iface.RegisterNatives = [](JNIEnv* env, jclass klass, JNINativeMethod const* methods_ptr, jint count) {
+        using namespace jni::interface;
+
+        std::span methods {methods_ptr, count};
+        auto clsname = get_jni_class_name(env, klass);
+
+        for (auto method : methods) {
+            logf(ANDROID_LOG_VERBOSE, "Unity registering native on %s: %s %s @ 0x%p", 
+                    clsname.data(), method.name, method.signature, method.fnPtr);
+        }
+
+        return invoke_original(env, &JNINativeInterface::RegisterNatives, klass, methods_ptr, count);
+    };
+
+    return iface;
 }
 
 extern "C" void modloader_accept_unity_handle(void* uhandle) noexcept {
---
+++ libmodloader/src/tinynew.cpp
@@ -0,0 +1,62 @@
+/* tinynew.cpp
+   
+   Overrides operators new and delete
+   globally to reduce code size.
+   
+   Public domain, use however you wish.
+   If you really need a license, consider it MIT:
+   http://www.opensource.org/licenses/mit-license.php
+   
+   - Eric Agan
+     Elegant Invention
+ */
+ 
+#include <new>
+#include <malloc.h>
+#include "log.hpp"
+ 
+void* operator new(std::size_t size) {
+    return malloc(size);
+}
+ 
+void* operator new[](std::size_t size) {
+    return malloc(size);
+}
+ 
+void operator delete(void* ptr) {
+    free(ptr);
+}
+ 
+void operator delete[](void* ptr) {
+    free(ptr);
+}
+ 
+/* Optionally you can override the 'nothrow' versions as well.
+   This is useful if you want to catch failed allocs with your
+   own debug code, or keep track of heap usage for example,
+   rather than just eliminate exceptions.
+ */
+ 
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept {
+    return malloc(size);
+}
+ 
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
+    return malloc(size);
+}
+ 
+void operator delete(void* ptr, const std::nothrow_t&) noexcept {
+    free(ptr);
+}
+ 
+void operator delete[](void* ptr, const std::nothrow_t&) noexcept {
+    free(ptr);
+}
+
+/*
+    Additional stuff to help reduce binary size
+*/
+extern "C" void __cxa_pure_virtual() {
+    log(ANDROID_LOG_ERROR, "Pure virtual function call");
+    while (1);
+}
